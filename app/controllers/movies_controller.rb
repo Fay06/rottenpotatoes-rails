398730d9053c4e134729e02a5a93c697e1cd73a5
Yufei Liu <yl5099@columbia.edu>
2022-10-04 00:59:57 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +7,14 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if params[:ratings] == nil
+      @movies = Movie.all
+      @ratings_to_show = []
+    else
+      @movies = Movie.where(rating: params[:ratings].keys)
+      @ratings_to_show = params[:ratings].keys
+    end
+      @all_ratings = Movie.all_ratings 
   end
 
   def new
